d631436dbd2c92347601ad0b71bd0b3c684eb699
Dmytrenko-Roman <savitarreus@gmail.com>
2022-09-06 16:04:37 +0300

    ❄️ Added engine_type field to Car model ❄️

--- car_sales/api/models.py
+++ car_sales/api/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from api.constants.car_fields import ENGINE_TYPE_CHOICES
+
 
 class CustomUser(AbstractUser):
     def __str__(self):
@@ -41,8 +43,9 @@ class Car(models.Model):
     engine_volume = models.DecimalField(
         max_digits=2, decimal_places=1, default=0
     )
+    engine_type = models.CharField(max_length=20, choices=ENGINE_TYPE_CHOICES, null=True)
     horsepower = models.PositiveIntegerField(null=False)
-    # TODO: add color and engine_type fields
+    # TODO: add color field
     owners_count = models.PositiveIntegerField(default=1)
     seats_count = models.PositiveIntegerField(null=False)
     doors_count = models.PositiveIntegerField(null=False)
--- car_sales/api/serializers.py
+++ car_sales/api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 
-from api import constants
+from api.constants import model_types
 
 
 class UploadSerializer(serializers.Serializer):
     file_uploaded = serializers.FileField()
     model_type = serializers.ChoiceField(
-        choices=constants.MODEL_TYPE_CHOICES,
+        choices=model_types.MODEL_TYPE_CHOICES,
     )
 
     class Meta:
