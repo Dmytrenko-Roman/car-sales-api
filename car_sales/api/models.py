f1d4e153f02d8c54ce450438e7470074177c5fe3
Dmytrenko-Roman <savitarreus@gmail.com>
2022-09-06 14:49:58 +0300

    ❄️ Created model for car ❄️

--- car_sales/api/models.py
+++ car_sales/api/models.py
@@ -8,21 +8,21 @@ class CustomUser(AbstractUser):
 
 
 class CarType(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null=False)
 
     def __str__(self):
         return self.name
 
 
 class CarBrand(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null=False)
 
     def __str__(self):
         return self.name
 
 
 class CarModel(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, null=False)
 
     type = models.ForeignKey(to=CarType, on_delete=models.CASCADE, null=False)
     brand = models.ForeignKey(
@@ -31,3 +31,25 @@ class CarModel(models.Model):
 
     def __str__(self):
         return f"{self.brand} {self.name}"
+
+
+class Car(models.Model):
+    price = models.PositiveIntegerField(null=False)
+    mileage = models.PositiveIntegerField(null=False)
+    year = models.CharField(max_length=30, null=False)
+    description = models.CharField(max_length=500, null=True)
+    engine_volume = models.DecimalField(
+        max_digits=2, decimal_places=1, default=0
+    )
+    horsepower = models.PositiveIntegerField(null=False)
+    # TODO: add color and engine_type fields
+    owners_count = models.PositiveIntegerField(default=1)
+    seats_count = models.PositiveIntegerField(null=False)
+    doors_count = models.PositiveIntegerField(null=False)
+
+    model = models.ForeignKey(
+        to=CarModel, on_delete=models.CASCADE, null=False
+    )
+
+    def __str__(self):
+        return f"{str(self.model)}: ${str(self.price)}"
