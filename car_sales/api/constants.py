47a46c6b0e7aa77b6690520f750673e05379b8fd
Dmytrenko-Roman <savitarreus@gmail.com>
2022-09-06 15:45:12 +0300

    ❄️ Created constants.py ❄️

---
+++ car_sales/api/constants.py
@@ -0,0 +1,15 @@
+from api.models import CarBrand, CarModel, CarType
+
+# Constants for file uploading:
+
+MODEL_TYPES = {
+    "car_brand": CarBrand,
+    "car_model": CarModel,
+    "car_type": CarType,
+}
+
+MODEL_TYPE_CHOICES = (
+    ("car_brand", "Car Brand"),
+    ("car_model", "Car Model"),
+    ("car_type", "Car Type"),
+)
